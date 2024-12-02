2299eac309befeb81b2cf16b7c6d1bc9fd2725d8
KirillRV <kirillreges639@gmail.com>
2024-12-02 23:37:48 +0300

    Authentication Commit

---
+++ src/main/com/teachmeskills/final_assignment/session/Session.java
@@ -0,0 +1,22 @@
+package main.com.teachmeskills.final_assignment.session;
+
+import java.time.LocalDateTime;
+
+
+public class Session {
+    private String username;
+    private LocalDateTime expiryTime;
+
+    public Session(String username, int sessionDurationMinutes) {
+        this.username = username;
+        this.expiryTime = LocalDateTime.now().plusMinutes(sessionDurationMinutes);
+    }
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expiryTime);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
