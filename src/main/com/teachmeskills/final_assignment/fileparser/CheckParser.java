a7ccaa95a6d469c484527c4984d61bd3cadc061e
Marharyta Amosava <mozheliukrita@gmail.com>
2024-12-02 18:35:43 +0100

    TMS Final Assignment
    Updated:
    Added BaseParser abstract class to avoid code duplicate

--- src/main/com/teachmeskills/final_assignment/fileparser/CheckParser.java
+++ src/main/com/teachmeskills/final_assignment/fileparser/CheckParser.java
@@ -1,35 +1,11 @@
 package main.com.teachmeskills.final_assignment.fileparser;
 
 import main.com.teachmeskills.final_assignment.model.Check;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
-public class CheckParser implements Parser<Check> {
+public class CheckParser extends BaseParser<Check> implements Parser<Check> {
 
-    @Override
-    public List<Check> parseFiles(List<Path> files) {
-        List<Check> checks = new ArrayList<>();
-
-        for (Path file : files) {
-            try {
-                List<String> lines = Files.readAllLines(file);
-                lines.removeIf(String::isBlank);
-                lines.replaceAll(String::trim);
-                Check check = analyzeTextFromFile(lines);
-                checks.add(check);
-            } catch (IOException e) {
-                System.err.println("Error during file reading: " + file.getFileName());
-            }
-        }
-
-        return checks;
-    }
-
-    private static Check analyzeTextFromFile(List<String> parsedText) {
+    protected Check analyzeTextFromFile(List<String> parsedText) {
 
         double checkAmount = 0;
 
--- src/main/com/teachmeskills/final_assignment/fileparser/InvoiceParser.java
+++ src/main/com/teachmeskills/final_assignment/fileparser/InvoiceParser.java
@@ -1,35 +1,12 @@
 package main.com.teachmeskills.final_assignment.fileparser;
 
 import main.com.teachmeskills.final_assignment.model.Invoice;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
-public class InvoiceParser implements Parser<Invoice> {
+public class InvoiceParser extends BaseParser<Invoice> implements Parser<Invoice> {
 
     @Override
-    public List<Invoice> parseFiles(List<Path> files) {
-        List<Invoice> invoices = new ArrayList<>();
-
-        for (Path file : files) {
-            try {
-                List<String> lines = Files.readAllLines(file);
-                lines.removeIf(String::isBlank);
-                lines.replaceAll(String::trim);
-                Invoice invoice = analyzeTextFromFile(lines);
-                invoices.add(invoice);
-            } catch (IOException e) {
-                System.err.println("Error during file reading: " + file.getFileName());
-            }
-        }
-
-        return invoices;
-    }
-
-    private static Invoice analyzeTextFromFile(List<String> parsedText) {
+    protected Invoice analyzeTextFromFile(List<String> parsedText) {
 
         double invoiceAmount = 0;
 
